c54b3c127684975b58f129446438372d29a2f334
Meghana <meghanadamerla2308@gmail.com>
2020-03-13 01:55:52 -0700

    EmartProject-Microservice

---
+++ Emart-payment-service/src/main/java/com/cts/eps/EmartPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmartPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmartPaymentServiceApplication.class, args);
+	}
+
+}
